205d6ccf4f125a9059f8008242b2dc9c9d3ae81a
RIHAN AHMAD <101428781+Rihanahmad@users.noreply.github.com>
2022-05-21 03:09:48 +0530

    Add files via upload

---
+++ sql oprators.sql
@@ -0,0 +1,14 @@
+
+use rihan
+
+select*from ahmad
+
+select naam,city from ahmad 
+
+select naam from ahmad where city='sbd'
+
+select * from ahmad where city='sbd' and naam='sameer'
+
+select * from ahmad where city='sbd' and (city='sbd' or city='saifni')
+
+
